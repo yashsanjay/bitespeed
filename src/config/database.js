59bcf2008defe8b44a77e1716b9493ef6613ad4c
YASH <yashsanjay.k21@iiits.in>
2025-02-11 01:10:20 +0530

    basic file setup and code

---
+++ src/config/database.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Database connection failed:", err.message);
+  } else {
+    console.log("Connected to MySQL database!");
+    connection.release();
+  }
+});
+
+module.exports = pool.promise();
